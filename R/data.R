27533c0f3078cacaa7ba5aa1235799bf64bab934
Stefan Müller <mullers@tcd.ie>
2018-03-05 21:42:56 +0000

    Add documentation for data_char_testphrases

--- R/data.R
+++ R/data.R
@@ -99,3 +99,15 @@
 #' @keywords data
 "data_dictionary_RID"
 
+
+#' Sample short documents for testing
+#' 
+#' Some sample short documents in plain text format for testing
+#'   with \code{\link{liwcalike}}.
+#' @examples
+#' liwcalike(data_char_testphrases)
+"data_char_testphrases"
+
+# save(testphrases, file = "data/testphrases.RData")
+# writeLines(testphrases, "inst/extdata/testphrases.txt")
+
